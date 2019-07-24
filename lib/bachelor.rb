a263edca873a43a8add77db062e795de17a3fa7d
kellyeryan <kellyeryan@gmail.com>
2019-07-24 17:54:59 -0400

    My solution

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,49 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |people|
+    people.each do |key, value|
+      if value == "Winner"
+        first_name = people["name"]
+        return first_name.split(' ').first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |_season, infos|
+    infos.each do |key|
+      if key["occupation"] == occupation
+        return key["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |_season, infos|
+    infos.each do |key|
+      if key["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |_season, infos|
+    infos.each do |key|
+      return key['occupation'] if key['hometown'] == hometown
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = data[season].map do |contestant|
+    contestant["age"].to_i
+  end
+  (ages.sum / ages.length.to_f).round
 end
